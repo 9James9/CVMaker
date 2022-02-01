fe45616b673c7b66a11f082843c4acc9287eee9b
James Driscoll <thejamesdriscoll@gmail.com>
2022-02-01 16:10:42 -0600

    company name and start date now appear on screen

--- src/components/workhistorydisplay.js
+++ src/components/workhistorydisplay.js
@@ -1,12 +1,39 @@
 import React from "react";
 
 export function WorkHistoryDisplay({ history }) {
+    let companies = []
+    let startDates = []
+    let endDates = []
+    function logCompany() {
+
+            for (let i = 0; i < Object.keys(history.jobs).length; i++) {
+                console.log(history.jobs[i].Company)
+                
+                    companies.push(`Worked at ${history.jobs[i].Company}`)
+                
+            }
+            for (let i = 0; i < Object.keys(history.jobs).length; i++) {
+                startDates.push(`${history.jobs[i].Start}`)
+            }
+        console.log(companies)
+    }
+    const displayJobs = () => {
+        return(
+            companies.map(company => {
+                <div>{company}</div>
+            })
+        )
+    }
   return (
     <div>
       <h2>Hello Workhistorydisplay</h2>
-      {/* {Object.keys(history.jobs).map((key,value) => {
-          return console.log(key,value)
-      })} */}
+      {console.log(history)}
+      <button onClick={logCompany}>Click me</button>
+      {logCompany()}
+      {companies.map((i,x) => {
+          return (<div key={x}>{i}{`Start Date: ${startDates[x]}`}
+          </div>)
+      })}
     </div>
   );
 }
