c00c0e11ada1d00ccf8dbd0679bed3ee79bd37de
James Driscoll <thejamesdriscoll@gmail.com>
2022-02-04 11:35:17 -0600

    set up skillsinput and skillsdisplay template

---
+++ src/components/Displays/Personal/Skills/skillsdisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './skillsdisplay.css'
+
+const SkillsDisplay = () => {
+  return (
+      <div>
+          <h5>Hello skillsdisplay</h5>
+      </div>
+  );
+};
+
+export default SkillsDisplay;
---
+++ src/components/inputs/Personal/Skills/skillsinput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './skillsinput.css'
+
+const SkillsInput = () => {
+  return (
+      <div className='skillsinput__container'>
+          <h5>Hello skillsinput</h5>
+      </div>
+  );
+};
+
+export default SkillsInput;
