15783e07f5cc25587c5db8bc160be83ae3d81ca2
James Driscoll <thejamesdriscoll@gmail.com>
2022-02-03 22:05:50 -0600

    workhistoryinput becomes column on mobile

--- src/components/inputs/Work/workhistoryinput.js
+++ src/components/inputs/Work/workhistoryinput.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './workhistoryinput.css'
 export function JobHistoryInput ({updateJobHistory,updateJobHistoryStart,updateJobHistoryEnd}) {
         return(
-            <div>
+            <div className='workhistoryinput__container'>
             <input type="text" placeholder='Company Name' onChange={(e) => {updateJobHistory(e)}}/>
             <input type="number" placeholder='Start Date' onChange={((e) => {updateJobHistoryStart(e)})}/>
             <input type="number" placeholder="End Date" onChange={((e) => {updateJobHistoryEnd(e)})}/>
