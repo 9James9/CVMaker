b1e7591d2efc4823ca079b491f48793d073f63f3
James Driscoll <thejamesdriscoll@gmail.com>
2022-02-04 13:33:08 -0600

    created project display and input files

---
+++ src/components/Displays/Projects/projectdisplay.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './projectdisplay.css'
+
+const ProjectDisplay = () => {
+  return (
+      <div className='projectdisplay__container'>
+          Hello projectdisplay
+      </div>
+  );
+};
+
+export default ProjectDisplay;
---
+++ src/components/inputs/Projects/projectinput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './projectinput.css'
+
+const ProjectInput = () => {
+  return (
+      <div className='projectinput__container'>
+          Hello projectinput
+      </div>
+  );
+};
+
+export default ProjectInput;
