81b4275c22c743600cc79d6be2b21cf231ad1c2f
James Driscoll <thejamesdriscoll@gmail.com>
2022-02-05 13:22:21 -0600

    added values to pdf

--- src/components/Pdf/pdfmaker.js
+++ src/components/Pdf/pdfmaker.js
@@ -1,15 +1,27 @@
 import jsPDF from 'jspdf';
 import React from 'react';
 
-const PdfMaker = () => {
+const PdfMaker = ({name,job,email,phone, github}) => {
+    const saveState = () => {
+        
+    }
     const makeDoc = e => {
         let doc = new jsPDF()
-        doc.text(20, 20, 'Hello World')
-        doc.save('Helloworld.pdf')
+        doc.setFontSize(48)
+        doc.text(20, 20, `${name}`)
+        doc.setFontSize(18)
+
+        doc.text(20,40,`${email}`)
+        doc.text(20,30,`${job}`)
+        doc.text(20,50, `${phone}`)
+        doc.text(120,30, `github.com/${github}`)
+
+        doc.save(`deletethis69.pdf`)
     }
   return (
       <div>
           <h2>Hello JSPDF</h2>
+          <button onClick={saveState}>Submit</button>
           <button onClick={makeDoc}>Save PDF</button>
       </div>
   );
