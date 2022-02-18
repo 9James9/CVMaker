d3714c30bd0e19f2d0917aea0521f04e1cdaa9be
James Driscoll <thejamesdriscoll@gmail.com>
2022-02-18 11:46:32 -0600

    added footer with github icon

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react'
 import './App.css'
 import {Main} from './components/Main/main'
 import Header from './components/Header/header'
+import Footer from './components/Footer/footer'
 function App() {
   return (
     <div className='app-container'>
       <Header/>
       <Main/>
+      <Footer/>
     </div>
   )
 }
